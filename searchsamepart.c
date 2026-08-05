046d51354dec7aa9f45ef09c822338cc583e5039
aarushiii08 <aarushi.jaincse24@ssipmt.com>
2026-08-05 23:49:44 +0530

    Add files via upload

---
+++ searchsamepart.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    while (left<=right) {
+        int mid = left + (right - left)/2;
+        if(nums[mid]==target){
+            return mid;
+        }
+        if (nums[left])
+    }
+} 
